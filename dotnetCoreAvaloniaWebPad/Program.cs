150a49a5225e3dfb936b3caa66baaa84de3e70de
Nathaniel Collier <nathaniel.collier@aecc.com>
2018-07-30 08:03:17 -0500

    Add project files.

---
+++ dotnetCoreAvaloniaWebPad/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetCoreAvaloniaWebPad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
